56000a3d8eca87aabd1c380a0902f0be79193ddb
Vaishu.B <89288843+Vaishalibhor@users.noreply.github.com>
2024-04-04 15:02:06 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+#1.
 import java.util.Arrays;
 
  class JavaTest {
@@ -33,5 +34,93 @@ import java.util.Arrays;
         int[] Y = {1, 8, 9, 10, 15};
         mergeArrays(X, Y);
         System.out.println("Merged array: " + Arrays.toString(X));
+    }
+}
+#2.
+ import java.util.Arrays;
+
+    class MaximumSumPath {
+    public static int maxSumPath(int[] X, int[] Y) {
+        int m = X.length;
+        int n = Y.length;
+        int sumX = 0, sumY = 0, result = 0;
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (X[i] < Y[j]) {
+                sumX += X[i++];
+            }
+            else if (X[i] > Y[j]) {
+                sumY += Y[j++];
+            }
+            else {
+                result += Math.max(sumX, sumY);
+                result += X[i]; // Add common element
+                sumX = 0;
+                sumY = 0;
+                i++;
+                j++;
+            }
+        }
+        while (i < m) {
+            sumX += X[i++];
+        }
+        while (j < n) {
+            sumY += Y[j++];
+        }
+        result += Math.max(sumX, sumY);
+
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] X = {3, 6, 7, 8, 10, 12, 15, 18, 100};
+        int[] Y = {1, 2, 3, 5, 7, 9, 10, 11, 15, 16, 18, 25, 50};
+        System.out.println("Maximum sum path: " + maxSumPath(X, Y));
+    }
+}
+#3.
+ import java.util.HashMap;
+
+ class WordCount {
+    public static void main(String[] args) {
+        java.lang.String str = "Hello world, this is a sample string.";
+        HashMap<String, Integer> wordCountMap = new HashMap<>();
+        String[] words = str.split("\\s+");
+        for (String word : words) {
+            if (wordCountMap.containsKey(word)) {
+                wordCountMap.put(word, wordCountMap.get(word) + 1);
+            } else {
+                wordCountMap.put(word, 1);
+            }
+        }
+        System.out.println("Word Count:");
+        for (String word : wordCountMap.keySet()) {
+            System.out.println(word + ": " + wordCountMap.get(word));
+        }
+    }
+}
+
+#4.
+ import java.util.HashMap;
+
+ class DuplicateCharacters {
+    public static void main(String[] args) {
+        java.lang.String str = "Hello, world!";
+        HashMap<Character, Integer> charCountMap = new HashMap<>();
+        char[] chars = str.toCharArray();
+        for (char ch : chars) {
+            if (Character.isLetter(ch)) {
+                if (charCountMap.containsKey(ch)) {
+                    charCountMap.put(ch, charCountMap.get(ch) + 1);
+                } else {
+                    charCountMap.put(ch, 1);
+                }
+            }
+        }
+        System.out.println("Duplicate Characters:");
+        for (char ch : charCountMap.keySet()) {
+            if (charCountMap.get(ch) > 1) {
+                System.out.println(ch + ": " + charCountMap.get(ch));
+            }
+        }
     }
 }
